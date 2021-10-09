0897fae6f2313bec6a564f8339980eb919e2b7fa
Dasha Syvyuk <dashasyvyuk@Dashas-MacBook-Pro.local>
2021-10-10 00:28:12 +0300

    not change photo

--- assets/home.js
+++ assets/home.js
@@ -25,16 +25,6 @@ $("#new-products").slick({
     pauseOnHover: false
 });
 
-$(document).on('mouseover', '.product .inner img', (e) => {
-    const next = $(e.currentTarget).data('next');
-    $(e.currentTarget).attr('src', next);
-});
-
-$(document).on('mouseout', '.product .inner img', (e) => {
-    const first = $(e.currentTarget).data('first');
-    $(e.currentTarget).attr('src', first);
-});
-
 $(document).on('mouseover', '.add2cart img', (e) => {
     $(e.currentTarget).attr('src', '/images/cart_active.png');
 });
