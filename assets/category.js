52a539774dc1fb176a499a53a707d2124d72898a
Dasha Syvyuk <syvyuk@ukr.net>
2023-06-18 16:36:32 +0300

    add category page

--- assets/category.js
+++ assets/category.js
@@ -11,7 +11,7 @@ $('#filters input[type=checkbox]').on('change', (e) => {
     window.location.replace(newUrl);
 });
 
-$(document).on('click', '#filter-form .selected-filters .item', (e) => {
+$(document).on('click', '#filter-form .selected-filters .item, #filters-mobile-form .selected-filters .item', (e) => {
     const filter = $(e.currentTarget).data('filter-id');
 
     if (filter == 'price_to') {
@@ -20,6 +20,7 @@ $(document).on('click', '#filter-form .selected-filters .item', (e) => {
         $('#price_from').val('').trigger('change');
     } else {
         $('#filters input[name='+filter+']').trigger('click');
+        $('#mobile-filters input[name='+filter+']').trigger('click');
     }
 
     $(e.currentTarget).remove();
@@ -59,11 +60,13 @@ function getUrl() {
 }
 
 function getOrderBy() {
-    const option = $('#products-block .order-by select').children("option:selected");
+    const option = $('#products-block .order-by select').length ?
+        $('#products-block .order-by select').children('option:selected') :
+        $('#mobile-sorting input:checked');
     const value = option.val();
     const direction = option.data('direction');
 
-    if (value.length && direction.length) {
+    if (value && value.length && direction.length) {
         return 'order=' + value + '&direction=' + direction;
     }
 
@@ -71,7 +74,7 @@ function getOrderBy() {
 }
 
 function getFilter() {
-    const form = $('#filter-form').serialize();
+    const form = $('#filter-form').serialize().length ? $('#filter-form').serialize() : $('#filters-mobile-form').serialize();
     const query = form.split('&');
     let attributes = query.map((attribute) => {
         let item = attribute.split('=');
@@ -88,7 +91,9 @@ function getFilter() {
 }
 
 function getPriceFrom() {
-    const target = $('#filters input[name=price_from]');
+    const target = $('#filters input[name=price_from]').length ?
+        $('#filters input[name=price_from]') :
+        $('#mobile-filters input[name=price_from]');
     const value = target.val();
 
     if (value.length) {
@@ -99,7 +104,9 @@ function getPriceFrom() {
 }
 
 function getPriceTo() {
-    const target = $('#filters input[name=price_to]');
+    const target = $('#filters input[name=price_to]').length ?
+        $('#filters input[name=price_to]') :
+        $('#mobile-filters input[name=price_to]');
     const value = target.val();
 
     if (value.length) {
@@ -142,7 +149,7 @@ $('.rating').each(function() {
     });
 });
 
-$('#filters .filter-title, #filters .show-all').on('mouseover', (e) => {
+$('#filters .filter-title, #filters .show-all, #mobile-filters .filter-title, #mobile-filters .show-all').on('mouseover', (e) => {
     if ($(e.currentTarget).find('img').attr('src') == '/images/arrow_down_gray.png') {
         $(e.currentTarget).find('img').attr('src', '/images/arrow_down_black.png');
     } else if ($(e.currentTarget).find('img').attr('src') == '/images/arrow_up_gray.png') {
@@ -150,7 +157,7 @@ $('#filters .filter-title, #filters .show-all').on('mouseover', (e) => {
     }
 });
 
-$('#filters .filter-title, #filters .show-all').on('mouseout', (e) => {
+$('#filters .filter-title, #filters .show-all, #mobile-filters .filter-title, #mobile-filters .show-all').on('mouseout', (e) => {
     if ($(e.currentTarget).find('img').attr('src') == '/images/arrow_down_black.png') {
         $(e.currentTarget).find('img').attr('src', '/images/arrow_down_gray.png');
     } else if ($(e.currentTarget).find('img').attr('src') == '/images/arrow_up_black.png') {
@@ -158,7 +165,7 @@ $('#filters .filter-title, #filters .show-all').on('mouseout', (e) => {
     }
 });
 
-$('#filters .filter-title').on('click', (e) => {
+$('#filters .filter-title, #mobile-filters .filter-title').on('click', (e) => {
     if ($(e.currentTarget).parent().find('.outer').hasClass('active') || $(e.currentTarget).parent().find('.outer').css('display') == 'block') {
         $(e.currentTarget).parent().find('.outer').removeClass('active');
         $(e.currentTarget).parent().find('.outer').slideUp();
@@ -170,7 +177,7 @@ $('#filters .filter-title').on('click', (e) => {
     }
 });
 
-$('#filters .show-all').on('click', (e) => {
+$('#filters .show-all, #mobile-filters .show-all').on('click', (e) => {
     if ($(e.currentTarget).parent().find('.hidden-part').hasClass('active')) {
         $(e.currentTarget).parent().find('.hidden-part').removeClass('active');
         $(e.currentTarget).parent().find('.hidden-part').slideUp();
@@ -180,4 +187,31 @@ $('#filters .show-all').on('click', (e) => {
         $(e.currentTarget).parent().find('.hidden-part').addClass('active');
         $(e.currentTarget).find('img').attr('src', '/images/arrow_up_gray.png');
     }
+});
+
+$('#show-filter').on('click', () => {
+    $('.filter-buttons').hide();
+    $('#mobile-filters').slideDown();
+    $('#products-block').hide();
+});
+
+$('#show-sort').on('click', () => {
+    $('.filter-buttons').hide();
+    $('#mobile-sorting').slideDown();
+    $('#products-block').hide();
+});
+
+$('.cancel').on('click', () => {
+    $(window).scrollTop(0);
+    $('.filter-buttons').show();
+    $('#mobile-sorting').hide();
+    $('#mobile-filters').hide();
+    $('#products-block').show();
+});
+
+$('.use').on('click', () => {
+    const url = window.location.pathname;
+    const newUrl = url + getUrl();
+
+    window.location.replace(newUrl);
 });
