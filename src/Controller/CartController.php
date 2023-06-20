c88859ae5469c66cd5151f86513e5e71d0ee7914
roma <roma-1994@ukr.net>
2023-06-20 16:46:33 +0300

    save progress

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,16 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\SettingRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class CartController extends AbstractController
+class CartController extends BaseController
 {
-    private ProductRepository $productRepository;
-
-    public function __construct(ProductRepository $productRepository) {
-        $this->productRepository = $productRepository;
+    public function __construct(
+        private readonly CategoryRepository $categoryRepository,
+        private readonly SettingRepository $settingRepository,
+        private readonly ProductRepository $productRepository
+    ) {
+        parent::__construct($this->categoryRepository, $this->settingRepository);
     }
 
     public function getCart(): Response
@@ -42,5 +46,33 @@ class CartController extends AbstractController
                 'totalPrice' => $totalPrice
             ])
         ]));
+    }
+
+    public function getMobileCart(Request $request): Response
+    {
+        $products = [];
+        $totalCount = 0;
+        $totalPrice = 0;
+
+        if (!empty($_COOKIE['cart'])) {
+            foreach (json_decode($_COOKIE['cart']) as $item) {
+                if ($item->id && $item->id > 0 && $item->count && $item->count > 0) {
+                    $product = $this->productRepository->findOneBy(['id' => $item->id]);
+
+                    if ($product) {
+                        $product->count = $item->count;
+                        $totalCount += $item->count;
+                        $totalPrice += $product->getPrice() * $item->count;
+                        $products[] = $product;
+                    }
+                }
+            }
+        }
+
+        return $this->renderTemplate($request, 'cart/index.html.twig', [
+            'products' => $products,
+            'totalCount' => $totalCount,
+            'totalPrice' => $totalPrice
+        ]);
     }
 }
