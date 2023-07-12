2ebebcbba532eee4343e48bdbb937672ede93c61
roma <roma-1994@ukr.net>
2023-07-12 04:18:14 +0300

    add link on properties and add order on index page for logged user

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use App\Entity\Product;
 use App\Repository\CategoryRepository;
 use App\Repository\FeedbackRepository;
+use App\Repository\OrderRepository;
 use App\Repository\ProductRepository;
 use App\Repository\SettingRepository;
 use App\Repository\SliderRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -31,21 +33,33 @@ class HomePageController extends BaseController
         CategoryRepository $categoryRepository,
         ProductRepository $productRepository,
         SettingRepository $settingRepository,
-        FeedbackRepository $feedbackRepository
+        FeedbackRepository $feedbackRepository,
+        UserRepository $userRepository,
+        OrderRepository $orderRepository
     ) {
         parent::__construct($categoryRepository, $settingRepository);
         $this->sliderRepository = $sliderRepository;
         $this->productRepository = $productRepository;
         $this->feedbackRepository = $feedbackRepository;
+        $this->userRepository = $userRepository;
+        $this->orderRepository = $orderRepository;
     }
 
     public function index(Request $request): Response
     {
+        $session = $request->getSession();
+
+        $email = $session->get('user');
+
+        $user = $this->userRepository->findOneBy(['email' => $email]);
+
         return $this->renderTemplate($request, 'home_page/index.html.twig', [
             'sliders' => $this->sliderRepository->findBy([], ['priority' => 'ASC']),
             'products' => $this->productRepository->findBy(['status' => Product::STATUS_ACTIVE], ['createdAt' => 'DESC'], 10),
             'totalCount' => $_COOKIE['totalCount'] ?? 0,
-            'feedbacks' => $this->feedbackRepository->findBy(['status' => 'CONFIRMED'], ['createdAt' => 'DESC'])
+            'feedbacks' => $this->feedbackRepository->findBy(['status' => 'CONFIRMED'], ['createdAt' => 'DESC']),
+            'order' => $this->orderRepository->findOneBy(['user' => $user], ['createdAt' => 'DESC']),
+            'user' => $user
         ]);
     }
 }
