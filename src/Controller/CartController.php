4ff3d6c93ec7f369ac0db6cea4bf191b47373b7f
Dasha Syvyuk <syvyuk@ukr.net>
2023-07-19 22:17:37 +0300

    fix cart error

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -20,6 +20,16 @@ class CartController extends BaseController
 
     public function getCart(): Response
     {
+        if (!array_key_exists('cart', $_COOKIE)) {
+            return new Response(json_encode([
+                'cart' => $this->renderView('cart/index.html.twig', [
+                    'products'   => [],
+                    'totalCount' => 0,
+                    'totalPrice' => 0
+                ])
+            ]));
+        }
+
         $cart = $this->cartShowService->run($_COOKIE['cart']);
 
         return new Response(json_encode([
@@ -33,9 +43,17 @@ class CartController extends BaseController
 
     public function getMobileCart(Request $request): Response
     {
+        if (!array_key_exists('cart', $_COOKIE)) {
+            return $this->renderTemplate($request, 'm/cart/index.html.twig', [
+                'products'   => [],
+                'totalCount' => 0,
+                'totalPrice' => 0
+            ]);
+        }
+
         $cart = $this->cartShowService->run($_COOKIE['cart']);
 
-        return $this->renderTemplate($request, 'cart/index.html.twig', [
+        return $this->renderTemplate($request, 'm/cart/index.html.twig', [
             'products'   => $cart['products'],
             'totalCount' => $cart['totalCount'],
             'totalPrice' => $cart['totalPrice']
