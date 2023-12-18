30c53b88c2f5a4c342f2ef6fcb6e46334cdd20b8
Dasha Syvyuk <syvyuk@ukr.net>
2023-12-18 19:35:10 +0200

    fix bug with &

--- src/Form/ProductCharacteristicType.php
+++ src/Form/ProductCharacteristicType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\ProductCharacteristic;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +13,8 @@ class ProductCharacteristicType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', null, ['label' => 'Назва', 'required' => true])
-            ->add('value', null, ['label' => 'Значення', 'required' => true])
+            ->add('title', TextType::class, ['label' => 'Назва', 'required' => true])
+            ->add('value', TextType::class, ['label' => 'Значення', 'required' => true])
             ->add('position', null, ['label' => 'Пріоритет', 'required' => true])
         ;
     }
