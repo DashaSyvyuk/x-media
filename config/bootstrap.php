3a3b3ff88dcc4d865895456da0f7bc79661a9d48
Dasha Syvyuk <35842053+DashaSyvyuk@users.noreply.github.com>
2026-05-08 19:24:12 +0300

    update bootstrap (#88)

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -2,31 +2,18 @@
 
 use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__).'/vendor/autoload.php';
 
 if (!class_exists(Dotenv::class)) {
-    throw new LogicException(
-        'Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.'
-    );
+    throw new LogicException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
 }
 
-// Load cached env vars if the .env.local.php file exists
-// Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
-if (
-    is_array($env = @include dirname(__DIR__) . '/.env.local.php') &&
-    (!isset($env['APP_ENV']) || ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? $env['APP_ENV']) === $env['APP_ENV'])
-) {
-    (new Dotenv(false))->populate($env);
-} elseif (! isset($env['APP_ENV'])) {
-    // load all the .env files
-    (new Dotenv(false))->loadEnv(dirname(__DIR__) . '/.env');
+if (!isset($_SERVER['APP_ENV']) && !isset($_ENV['APP_ENV'])) {
+    if (class_exists(Dotenv::class)) {
+        (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+    }
 }
 
-$_SERVER += $_ENV;
-$_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) ?: 'dev';
-$_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? 'prod' !== $_SERVER['APP_ENV'];
-$_SERVER['APP_DEBUG'] =
-    $_ENV['APP_DEBUG'] = (int) $_SERVER['APP_DEBUG'] || filter_var(
-        $_SERVER['APP_DEBUG'],
-        FILTER_VALIDATE_BOOLEAN
-    ) ? '1' : '0';
+if ($_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? false) {
+    umask(0000);
+}
