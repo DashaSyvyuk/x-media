b877fe12ab65860b98b34911504127028ad38f31
Dasha Syvyuk <35842053+DashaSyvyuk@users.noreply.github.com>
2026-05-08 19:41:37 +0300

    New bootstrap file (#90)

--- public/index.php
+++ public/index.php
@@ -1,9 +1,16 @@
 <?php
 
+$_SERVER['APP_RUNTIME_OPTIONS'] = [
+    'disable_dotenv' => true,
+];
+
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    return new Kernel(
+        $context['APP_ENV'] ?? 'prod',
+        (bool) ($context['APP_DEBUG'] ?? false)
+    );
 };
